6b5d96f1eee0276a902c8ede6ed4b48811576784
Naomi Amuzie <ncaa2018@mymail.pomona.edu>
2020-08-02 23:55:57 -0700

    added print statement

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        print("Hello")
     }
 
     @IBAction func onTap(_ sender: Any) {
